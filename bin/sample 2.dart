7f142d62dcd0a0eb0c857201d7543675db23384b
rinsha k <rinshakabeer14@gmail.com>
2022-09-19 13:59:43 +0530

    commit

---
+++ bin/sample 2.dart
@@ -0,0 +1,5 @@
+void main(){
+  int a =20;
+  int b =24;
+  print("the sum is ${a+b}.....the difference is${a-b}......the product is${a*b}");
+}
---
+++ bin/sample1.dart
@@ -0,0 +1,12 @@
+void main (){
+  String name = "hamda";
+  String lastname ="hyder";
+  int age= 22;
+  double markper = 3.6;
+ String address ="KALLUVALAPPIL";
+   print("my name is ${name}  ${lastname}.....iam ${age}yrs old......markper is ${markper}......adress  ${address}");
+
+
+
+
+}
